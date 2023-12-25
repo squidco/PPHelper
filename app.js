51b49fbf945afb4d310044c6434ef4ce2f4a3aa5
Anthony Pillow <anthonypillow18@gmail.com>
2023-12-25 00:15:54 -0800

    checking stuff

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.post("/interactions", async function (req, res) {
           type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
           data: {
             // Returns API response
-            content: playerInfo.response[0].player,
+            content: JSON.stringify(playerInfo.response[0].player),
           },
         });
       } catch (error) {
